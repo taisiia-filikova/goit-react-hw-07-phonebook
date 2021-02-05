6faacba60ed5c2bdf9cf55745146499bda09e08e
taisiia-filikova <filikova@gmail.com>
2021-02-05 13:03:36 +0200

    Изменяет компонент фильтр

--- src/components/ContactFilter/ContactFilter.js
+++ src/components/ContactFilter/ContactFilter.js
@@ -1,39 +1,37 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { filterContact } from '../../redux/contacts/contacts-actions';
-import {
-  getFilter,
-  getContacts,
-} from '../../redux/contacts/contacts-selectors';
-import { CSSTransition } from 'react-transition-group';
-import popTransition from '../../utils/transitions/pop.module.css';
+import { contactsActions, contactsSelectors } from '../../redux/contacts';
+import { motion, AnimatePresence } from 'framer-motion';
+import { variants } from '../../utils/motionVar';
 
 import s from './ContactFilter.module.css';
 
 function ContactFilter() {
   const dispatch = useDispatch();
-  const filter = useSelector(getFilter);
-  const contacts = useSelector(getContacts);
-
+  const filter = useSelector(contactsSelectors.getFilter);
+  const contacts = useSelector(contactsSelectors.getContacts);
+  const error = useSelector(contactsSelectors.getError);
   return (
-    <CSSTransition
-      in={contacts.length > 0}
-      timeout={250}
-      classNames={popTransition}
-      mountOnEnter
-      unmountOnExit
-    >
-      <label className={s.label}>
-        Find contacts by name
-        <input
-          className={s.input}
-          type="text"
-          value={filter}
-          // placeholder="Enter something"
-          onChange={evt => dispatch(filterContact(evt.target.value))}
-        />
-      </label>
-    </CSSTransition>
+    <>
+      {contacts.length > 0 && !error && (
+        <AnimatePresence>
+          <label className={s.label}>
+            <motion.input
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              transition="transition"
+              variants={variants}
+              className={s.input}
+              type="text"
+              value={filter}
+              onChange={evt =>
+                dispatch(contactsActions.filterContact(evt.target.value))
+              }
+            />
+          </label>
+        </AnimatePresence>
+      )}
+    </>
   );
 }
-
 export default ContactFilter;
